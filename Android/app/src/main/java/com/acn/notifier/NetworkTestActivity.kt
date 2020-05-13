0ef330ec34ab663059adc0dc466a3ca09d2eae55
Sebastian Knoll <sebastian.knoll@student.tugraz.at>
2020-05-13 11:20:38 +0200

    NetworkTest: some nice border and random button text

--- Android/app/src/main/java/com/acn/notifier/NetworkTestActivity.kt
+++ Android/app/src/main/java/com/acn/notifier/NetworkTestActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import android.os.StrictMode
 import android.util.JsonReader
 import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import org.json.JSONArray
 import org.json.JSONObject
 import java.io.InputStream
 import java.net.HttpURLConnection
 import java.net.URL
+import kotlin.random.Random
 
 
 class NetworkTestActivity : Activity() {
@@ -22,6 +24,7 @@ class NetworkTestActivity : Activity() {
         StrictMode.setThreadPolicy(policy)
 
         setContentView(R.layout.activity_network_test)
+        requestAndShowNewFact(null);
     }
 
     fun requestAndShowNewFact(view: View?) {
@@ -38,6 +41,12 @@ class NetworkTestActivity : Activity() {
         }
         println(responseFact);
 
-        findViewById<TextView>(R.id.serverMessage).text = responseFact;
+        findViewById<TextView>(R.id.serverMessage).text = responseFact
+        findViewById<Button>(R.id.buttonNewMessage).text = getRandomButtonText()
+    }
+
+    private fun getRandomButtonText():String {
+        val textSet = arrayOf("Go", "Next", "Tell me more", "Love it", "Go ahead", "Ok")
+        return textSet[Random.nextInt(textSet.size)]
     }
 }

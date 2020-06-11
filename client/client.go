57f08cf1cc8aa6dc4858fa7f62a64a1fe4146b5b
Marcel Nageler <marcel.nageler@student.tugraz.at>
2020-06-11 17:05:01 +0200

    client side sender token handling

--- client/client.go
+++ client/client.go
@@ -10,11 +10,15 @@ import (
 	"fmt"
 	"log"
 	"net/url"
+	"os"
 	"os/exec"
+	"path"
 
 	"github.com/gorilla/websocket"
 	"golang.org/x/crypto/sha3"
 )
+
+const mintokens int = 16
 
 var server = flag.String("server", "acn.nageler.org", "https service address")
 var port = flag.Int("port", 443, "remote port")
@@ -66,14 +70,72 @@ func authorize(conn *websocket.Conn, privkey ed25519.PrivateKey) error {
 	return nil
 }
 
+
+func topUpTokens(tokens *[]PrivateSenderToken, privkey ed25519.PrivateKey, conn *websocket.Conn, filepath string) error {
+	if len(*tokens) < mintokens {
+		diff := mintokens - len(*tokens)
+
+		log.Printf("generating %d new tokens\n", diff)
+		newtokens, err := generateTokens(diff, privkey)
+
+		if err != nil {
+			return err
+		}
+
+		token_copy := make([]PrivateSenderToken, mintokens)
+		copy(token_copy, *tokens)
+
+		token_copy = append(token_copy, newtokens...)
+		err = saveTokens(filepath, token_copy)
+		if err != nil {
+			return err
+		}
+
+		err = conn.WriteJSON(newtokens)
+		if err != nil {
+			return err
+		}
+
+		*tokens = token_copy
+	}
+
+	return nil
+}
+
+
 func main() {
 	flag.Parse()
 	log.SetFlags(0)
 
-	privatekey, err := loadKey("/home/marcel/.acnkey")
-	if err != nil {
+	cfg_path, err := os.UserConfigDir()
+	cfg_dir := path.Join(cfg_path, "notifier")
+	keypath := path.Join(cfg_dir, "key.json")
+	tokenpath := path.Join(cfg_dir, "tokens.json")
+
+	if _, err := os.Stat(cfg_dir); os.IsNotExist(err) {
+		os.Mkdir(cfg_dir, 0700)
+	}
+
+	privatekey, err := loadKey(keypath)
+	if os.IsNotExist(err) {
+		fmt.Printf("%s does not exist, generating a new one\n", keypath)
+		privatekey, err = generateAndSaveKey(keypath)
+		if err != nil {
+			log.Fatal("keygen failed: ", err)
+			os.Exit (1)
+		}
+	} else if err != nil {
 		log.Fatal("loadKey failed: ", err)
 	}
+
+	tokens, err := loadTokens(tokenpath)
+	if os.IsNotExist(err) {
+		tokens = []PrivateSenderToken{}
+	} else if err != nil {
+		log.Fatal("loadKey failed: ", err)
+		os.Exit (1)
+	}
+	fmt.Printf("read tokens: %s\n", tokens)
 
 	fmt.Printf("public key: %s\n", base64.StdEncoding.EncodeToString(privatekey.Public().(ed25519.PublicKey)))
 
@@ -98,7 +160,13 @@ func main() {
 		log.Fatal("authorize failed: ", err)
 	}
 
+	err = c.WriteJSON(tokens)
+
 	for {
+		if err := topUpTokens(&tokens, privatekey, c, tokenpath); err != nil {
+			log.Fatal("topUpTokens failure", err)
+			return
+		}
 		var msg SmallMessage
 		err := c.ReadJSON(&msg)
 		if err != nil {
--- client/keymgmt.go
+++ client/keymgmt.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"crypto/ed25519"
+	"crypto/subtle"
+	"crypto/rand"
 	"encoding/json"
 	"errors"
 	"io/ioutil"
+
+	"golang.org/x/crypto/curve25519"
 )
 
 type KeyStruct struct {
@@ -28,4 +32,101 @@ func loadKey(filePath string) (ed25519.PrivateKey, error) {
 	}
 
 	return ed25519.NewKeyFromSeed(keyStruct.PrivateKey), nil
+}
+
+func generateAndSaveKey(filePath string) (ed25519.PrivateKey, error) {
+	var keyStruct KeyStruct
+	keyStruct.PrivateKey = make([]byte, ed25519.SeedSize)
+
+
+	marshalled, err := json.MarshalIndent(keyStruct, "", "  ")
+
+	if err != nil {
+		return nil, err
+	}
+
+	if err := ioutil.WriteFile(filePath, marshalled, 0600); err != nil {
+		return nil, err
+	}
+
+	return ed25519.NewKeyFromSeed(keyStruct.PrivateKey), nil
+}
+
+func loadTokens(filePath string) ([]PrivateSenderToken, error) {
+	var tokens []PrivateSenderToken
+
+	bytes, err := ioutil.ReadFile(filePath)
+	if err != nil {
+		return nil, err
+	}
+	err = json.Unmarshal(bytes, &tokens)
+	if err != nil {
+		return nil, err
+	}
+
+	return tokens, nil
+}
+
+func generateTokens(numtokens int, privkey ed25519.PrivateKey) ([]PrivateSenderToken, error) {
+	tokens := make([]PrivateSenderToken, numtokens)
+
+
+	for i := range tokens {
+		var err error
+		tokens[i].Id = make([]byte, 32)
+		tokens[i].PrivateScalar = make([]byte, 32)
+
+		if _, err = rand.Read(tokens[i].Id[:]); err != nil {
+			return nil, err
+		}
+		if _, err = rand.Read(tokens[i].PrivateScalar[:]); err != nil {
+			return nil, err
+		}
+
+		tokens[i].PublicPoint, err = curve25519.X25519(tokens[i].PrivateScalar, curve25519.Basepoint)
+		if err != nil {
+			return nil, err
+		}
+
+		hash := HashTuple([]byte("sender tokens[i]"), tokens[i].Id, tokens[i].PublicPoint)
+
+		tokens[i].Signature = ed25519.Sign(privkey, hash)
+	}
+
+	return tokens, nil
+}
+
+func getToken(tokens *[]PrivateSenderToken, id []byte) (PrivateSenderToken, error) {
+	var result PrivateSenderToken
+
+	tokenIndex := -1
+	for i, token := range *tokens {
+		if subtle.ConstantTimeCompare(token.Id, id) == 1 {
+			tokenIndex = i
+			break
+		}
+	}
+
+	if tokenIndex == -1 {
+		return result, errors.New("Sender token not found")
+	}
+
+	result = (*tokens)[tokenIndex]
+	*tokens = append((*tokens)[:tokenIndex], (*tokens)[tokenIndex + 1:]...)
+
+	return result, nil
+}
+
+func saveTokens(filePath string, tokens []PrivateSenderToken) error {
+	marshalled, err := json.MarshalIndent(tokens, "", "  ")
+
+	if err != nil {
+		return err
+	}
+
+	if err := ioutil.WriteFile(filePath, marshalled, 0600); err != nil {
+		return err
+	}
+
+	return nil
 }
